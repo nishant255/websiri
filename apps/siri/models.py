19767d9c0a0a761ff3f04b4b1c69cd4433078284
Marcus Sakoda <Jubhub@yahool.com>
2017-01-30 18:47:04 -0800

    initial commit

---
+++ apps/siri/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Request(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Result(models.Model):
+    request = models.ForeignKey(Request)
+    user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
